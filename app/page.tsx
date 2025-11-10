d6c9bc16afeaa251b1bc8bb0e1f99e43764c4096
Andy Cheng <b10402113@gmail.com>
2025-11-11 07:34:41 +0800

    Assets and metadata

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="h1-bold">Tailwind CSS is fun!!</h1>
+      <h1 className="h1-bold font-space-grotest">Tailwind CSS is fun!!</h1>
+    </div>
+  );
+};
+
+export default Home;
