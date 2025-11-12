ff43e1801907ac7ef7922c2cd3c01b59d388c8e0
Andy Cheng <b10402113@gmail.com>
2025-11-12 09:09:31 +0800

    Setup Auth Layouts, Github and google providers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
