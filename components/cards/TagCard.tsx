b21ca0d02369bfb2e929c509b68cc639788c4c3b
Andy Cheng <b10402113@gmail.com>
2025-12-02 08:51:38 +0800

    Implement Local Searchbar

---
+++ components/cards/TagCard.tsx
@@ -0,0 +1,32 @@
+import ROUTES from "@/constants/routes";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
+import React from "react";
+import { getDeviconClassName } from "@/lib/utils";
+
+interface Props {
+  _id: string;
+  name: string;
+  questions: number;
+  showCount?: boolean;
+  compact?: boolean;
+}
+
+const TagCard = ({ _id, name, questions, showCount, compact }) => {
+  const iconClass = getDeviconClassName(name);
+  return (
+    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none space-x-0 uppercase">
+        <div className="flex-center space-x-2">
+          <i className={`${iconClass} tex-sm`}></i>
+          <span>{name}</span>
+        </div>
+      </Badge>
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{questions}</p>
+      )}
+    </Link>
+  );
+};
+
+export default TagCard;
--- constants/index.ts
+++ constants/index.ts
@@ -9,29 +9,29 @@ export const sidebarLinks = [
     route: "/community",
     label: "Community",
   },
-  {
-    imgURL: "/icons/star.svg",
-    route: "/collection",
-    label: "Collections",
-  },
-  {
-    imgURL: "/icons/suitcase.svg",
-    route: "/jobs",
-    label: "Find Jobs",
-  },
-  {
-    imgURL: "/icons/tag.svg",
-    route: "/tags",
-    label: "Tags",
-  },
-  {
-    imgURL: "/icons/user.svg",
-    route: "/profile",
-    label: "Profile",
-  },
-  {
-    imgURL: "/icons/question.svg",
-    route: "/ask-question",
-    label: "Ask a question",
-  },
+  //   {
+  //     imgURL: "/icons/star.svg",
+  //     route: "/collection",
+  //     label: "Collections",
+  //   },
+  //   {
+  //     imgURL: "/icons/suitcase.svg",
+  //     route: "/jobs",
+  //     label: "Find Jobs",
+  //   },
+  //   {
+  //     imgURL: "/icons/tag.svg",
+  //     route: "/tags",
+  //     label: "Tags",
+  //   },
+  //   {
+  //     imgURL: "/icons/user.svg",
+  //     route: "/profile",
+  //     label: "Profile",
+  //   },
+  //   {
+  //     imgURL: "/icons/question.svg",
+  //     route: "/ask-question",
+  //     label: "Ask a question",
+  //   },
 ];
